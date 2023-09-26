92223517aa825a1cd3d4e7b499997a57e8ff49c6
Omer <omarullahk@gmail.com>
2023-09-26 01:46:14 -0500

    Initial commit

---
+++ build/precache-manifest.6a9efd9841ff7976b6cf47de46638421.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3afc4d557111d3d500c8",
+    "url": "/DDC_Current/static/css/main.f984eb1a.chunk.css"
+  },
+  {
+    "revision": "3afc4d557111d3d500c8",
+    "url": "/DDC_Current/static/js/main.3afc4d55.chunk.js"
+  },
+  {
+    "revision": "11419f9ab822d4f34c57",
+    "url": "/DDC_Current/static/js/1.11419f9a.chunk.js"
+  },
+  {
+    "revision": "403e610496aea771df47",
+    "url": "/DDC_Current/static/css/2.2b2b589f.chunk.css"
+  },
+  {
+    "revision": "403e610496aea771df47",
+    "url": "/DDC_Current/static/js/2.403e6104.chunk.js"
+  },
+  {
+    "revision": "ed46da50b0339dc22265",
+    "url": "/DDC_Current/static/js/runtime~main.ed46da50.js"
+  },
+  {
+    "revision": "94c2c6cfecf50a19c26bdb59fbf74e1a",
+    "url": "/DDC_Current/index.html"
+  }
+];
